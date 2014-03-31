63911e334a3b1f86019c63f3b15fc37146a8e3d1
Yong-Soo Chung <yongsoochung@gmail.com>
2014-03-31 11:56:07 -0700

    [Gruntfile] Updated Gruntfile to add grunt-replace

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,55 +1,32 @@
-module.exports = function(grunt) {
+var path = require('path'),
+    fs = require('fs');
+
+var pkgFilename = 'package.json',
+    pkgFilePath = './ripple-client/';
+
+var pkgFile = path.join('pkgFilename', 'pkgFilePath');
+
+module.exports = function (grunt) {
+  grunt.loadNpmTasks('grunt-replace');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      options: {
-        separator: ';'
-      },
+    replace: {
       dist: {
-        src: ['src/**/*.js'],
-        dest: 'dist/<%= pkg.name %>.js'
+        options: {
+          patterns: [
+            {
+              match: 'version',
+              replacement: 'v'
+            }
+          ]
+        },
+        files: [
+          {expand: true, flatten: true, src: ['downloads.html'], dest: './'}
+        ]
       }
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      dist: {
-        files: {
-          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-        }
-      }
-    },
-    qunit: {
-      files: ['test/**/*.html']
-    },
-    jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-      options: {
-        // options here to override JSHint defaults
-        globals: {
-          jQuery: true,
-          console: true,
-          module: true,
-          document: true
-        }
-      }
-    },
-    watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint', 'qunit']
     }
   });
-
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-
-  grunt.registerTask('test', ['jshint', 'qunit']);
-
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
-
+  
+  grunt.registerTask('default', 'replace');
 };
